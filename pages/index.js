8f8e1391bfe60bcae164b3e610ec3315e96e87d8
soo <jstensix@naver.com>
2021-05-06 12:48:16 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,10 @@ import styles from '../styles/Home.module.scss'
 
 import Layout from './components/Layout'
 
-import s from '../styles/Test.scss';
 
 
 export default function Home() {
   return (
-    
       <Layout> 
         <div className="header">메인</div>
       </Layout>
