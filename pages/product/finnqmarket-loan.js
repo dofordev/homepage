8b75f4abff916452072820abd779e6cc1df373b1
Heodokyung <limewhale@daum.net>
2021-05-07 12:39:33 +0900

    이미지 추가

--- pages/product/finnqmarket-loan.js
+++ pages/product/finnqmarket-loan.js
@@ -1,9 +1,26 @@
 import Layout from "../components/Layout";
 
-export default function finnqmarketCard() {
+export default function FinnqmarketLoan() {
   return (
     <Layout>
-      <div>대출</div>
+      <div>
+        <div className="tab-wrap">
+          <ul>
+            <li class="is-active">
+              <button type="button">대출비교</button>
+            </li>
+            <li>
+              <button type="button">비상금대출</button>
+            </li>
+            <li>
+              <button type="button">똑똑대출</button>
+            </li>
+            <li>
+              <button type="button">생활비대출</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </Layout>
   );
 }
