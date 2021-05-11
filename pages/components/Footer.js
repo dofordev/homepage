9c3aeed01e182aa524dda32124c0206a63b86351
Heodokyung <limewhale@daum.net>
2021-05-11 12:12:48 +0900

    해더/풋터

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,6 +1,291 @@
-export default function Footer(){
-      
-    return (
-        <footer className="footer">푸터</footer>
-    )
+export default function Footer() {
+  // const isActive = document.getElementsByClassName('is-active');
+  // if (isActive.length > 0) {
+  //
+  // }
+
+  const menuActive = () => {
+    document.getElementById("family-list").classList.toggle("is-active");
+  };
+
+  return (
+    <footer className="footer">
+      <div className="width-fix">
+        <ul className="footer-menu">
+          <li>
+            <a
+              href="https://pop.finnq.com/finnq_home/files/Finnq_BI_Guide.pdf"
+              target="_blank"
+            >
+              B.I 가이드
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://pop.finnq.com/finnq_home/files/Finnq_brand_asset.zip"
+              target="_self"
+            >
+              로고 다운로드
+            </a>
+          </li>
+          <li>
+            <a href="/policy/service.html">서비스이용약관</a>
+          </li>
+          <li>
+            <a href="/policy/privacy.html">개인정보처리방침</a>
+          </li>
+          <li>
+            <a href="/policy/customer_information.html">고객정보취급방침</a>
+          </li>
+          <li>
+            <a href="/hfg/vision_group.html">하나금융그룹</a>
+          </li>
+        </ul>
+        <div className="info">
+          <p className="company-title">주식회사 핀크</p>
+          <ul className="company-list">
+            <li>
+              <address>서울시 중구 남대문로9길 40 센터플레이스 22층</address>
+            </li>
+            <li>대표이사 : 권영탁</li>
+            <li>사업자 등록번호 : 299-81-00502</li>
+            <li>통신판매업 신고번호 : 제 2017-서울중구-1146호</li>
+            <li>
+              <a
+                href="http://ftc.go.kr/bizCommPop.do?wrkr_no=2998100502"
+                target="_blank"
+                title="새창 열림"
+              >
+                사업자정보확인
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://pop.finnq.com/finnq_home/files/finnq2017.pdf"
+                target="_blank"
+                title="새창 열기"
+              >
+                재무제표공고(2017)
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://pop.finnq.com/finnq_home/files/finnq2018.pdf"
+                target="_blank"
+                title="새창 열기"
+              >
+                재무제표공고(2018)
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://pop.finnq.com/finnq_home/files/finnq2019.pdf"
+                target="_blank"
+                title="새창 열기"
+              >
+                재무제표공고(2019)
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://pop.finnq.com/finnq_home/files/finnq2020.pdf"
+                target="_blank"
+                title="새창 열기"
+              >
+                재무제표공고(2020)
+              </a>
+            </li>
+            <li>
+              <a href="/policy/announce.html" className="btn-layer">
+                선불충전금 운용공시(2021.1Q)
+              </a>
+            </li>
+            <li>호스팅서비스 사업자 : 주식회사 핀크</li>
+            <li>고객센터 : 1566-4949</li>
+            <li>
+              E-mail : <a href="mailto:cs@finnq.co.kr">cs@finnq.co.kr</a>
+            </li>
+            <li>
+              제휴문의 :{" "}
+              <a href="mailto:partner@finnq.co.kr">partner@finnq.co.kr</a>
+            </li>
+          </ul>
+          <p className="copyright">Copyright © Finnq Inc.</p>
+        </div>
+        <div className="family">
+          <button type="button" className="btn-family" onClick={menuActive}>
+            계열사
+          </button>
+          <ul className="family-list" id="family-list">
+            <li>
+              <a href="http://www.hanafn.com" target="_blank" title="새창 열림">
+                하나금융그룹
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.kebhana.com"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나은행
+              </a>
+            </li>
+            <li>
+              <a href="http://www.hanaw.com" target="_blank" title="새창 열림">
+                하나금융투자
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanacard.co.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나카드
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanacapital.co.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나캐피탈
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanalife.co.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나생명
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanasavings.com"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나저축은행
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanatrust.com"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나자산신탁
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hana-assetmanagement.com"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나대체투자자산운용
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanais.co.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나펀드서비스
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanati.co.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나금융티아이
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hana-nanum.com"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나금융나눔재단
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanafoundation.or.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나금융공익재단
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanacarecenter.or.kr"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나케어센터
+              </a>
+            </li>
+            <li>
+              <a
+                href="http://www.hanamiso.org"
+                target="_blank"
+                title="새창 열림"
+              >
+                하나미소금융재단
+              </a>
+            </li>
+            <li>
+              <a href="http://www.hana.hs.kr" target="_blank" title="새창 열림">
+                하나고등학교
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="sns-wrap">
+          <ul className="sns-list clearfix">
+            <li>
+              <a
+                href="https://www.facebook.com/Finnq.official/"
+                className="btn-sns btn-fb"
+                target="_blank"
+                title="새창 열림"
+              >
+                페이스북 공유하기
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/finnq_official/"
+                className="btn-sns btn-insta"
+                target="_blank"
+                title="새창 열림"
+              >
+                인스타그램 공유하기
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://post.naver.com/finnq"
+                className="btn-sns btn-blog"
+                target="_blank"
+                title="새창 열림"
+              >
+                네이버블로그 공유하기
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- pages/components/Header.js
+++ pages/components/Header.js
@@ -1,6 +1,144 @@
-export default function Header(){
-      
-    return (
-        <header className="header">헤더 테스트</header>
-    )
+export default function Header() {
+  const mouseEnter = () => {
+    document.querySelector("header").classList.add("fixed");
+  };
+  const mouseLeave = () => {
+    document.querySelector("header").classList.remove("fixed");
+  };
+
+  return (
+    <header
+      className="header"
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+    >
+      <div className="width-fix">
+        <h1 className="header-logo">
+          <a href="/index.html">Finnq</a>
+        </h1>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="finnqmarket">
+              <a href="/product/finnqmarket-card.html?tabNum=0&tabItem=0">
+                카드
+              </a>
+            </li>
+            <li className="finnqmarket">
+              <a href="/product/finnqmarket-saving.html?tabNum=0&tabItem=0">
+                예적금
+              </a>
+            </li>
+            <li className="finnqmarket">
+              <a href="/product/finnqmarket-loan.html?tabNum=0&tabItem=0">
+                대출
+              </a>
+            </li>
+            <li className="finnqmarket">
+              <a
+                href="https://loan.finnq.com/intro/finnq?pcn=WEB_Bridge"
+                target="_blank"
+              >
+                대출금리비교<i className="new"></i>
+              </a>
+            </li>
+            <li className="finnqmarket">
+              <a href="/product/finnqmarket-insurance.html?tabNum=0&tabItem=0">
+                보험 맞춤 추천
+              </a>
+            </li>
+            <li className="cerfi">
+              <a href="/cert/cert.html">공동인증센터</a>
+            </li>
+          </ul>
+        </nav>
+        <nav className="m-nav">
+          <button type="button" className="btn-nav">
+            메뉴
+          </button>
+        </nav>
+        <div className="m-gnb">
+          <a href="index.html" className="m-gnb-logo">
+            <span className="blind">finnq</span>
+          </a>
+          <div className="m-gnb-inner">
+            <ul>
+              <li>
+                <a href="/product/finnqmarket-card.html?tabNum=0&tabItem=0">
+                  금융상품
+                </a>
+                <ul className="m-sub-menu">
+                  <li>
+                    <a href="/product/finnqmarket-card.html?tabNum=0&tabItem=0">
+                      카드
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/product/finnqmarket-saving.html?tabNum=0&tabItem=0">
+                      예적금
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/product/finnqmarket-loan.html?tabNum=0&tabItem=0">
+                      대출
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://loan.finnq.com/intro/finnq?pcn=WEB_Bridge"
+                      target="_blank"
+                    >
+                      대출금리비교<i className="new"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/product/finnqmarket-insurance.html?tabNum=0&tabItem=0">
+                      보험 맞춤 추천
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="m-sns">
+            <ul>
+              <li>
+                <a
+                  href="https://www.facebook.com/Finnq.official/"
+                  className="btn-gnb-fb"
+                  target="_blank"
+                  title="새창 열림"
+                >
+                  페이스북 공유하기
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/finnq_official/"
+                  className="btn-gnb-insta"
+                  target="_blank"
+                  title="새창 열림"
+                >
+                  인스타그램 공유하기
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://post.naver.com/finnq"
+                  className="btn-gnb-blog"
+                  target="_blank"
+                  title="새창 열림"
+                >
+                  네이버블로그 공유하기
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button type="button" className="m-gnb-close">
+            <span className="blind">닫기</span>
+          </button>
+        </div>
+        <div className="m-gnb-deem"></div>
+      </div>
+    </header>
+  );
 }
